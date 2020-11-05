4ed1981e67869d57c7a90356f596ccdf0e472eda
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-11-05 14:22:32 -0500

    Added Vatsimbrief Helper Github URL

--- build_configuration.cmd
+++ build_configuration.cmd
@@ -1,3 +1,3 @@
 set READABLE_SCRIPT_NAME="Vatsimbrief Helper"
 set RELEASE_FILE_NAME_PREFIX="vatsimbrief-helper"
-set GITHUB_REPO_URL="https://github.com/____UPDATE_REPO_URL_in_build_configuration_cmd"
+set GITHUB_REPO_URL="https://github.com/RedXi/vatsimbrief-helper"
