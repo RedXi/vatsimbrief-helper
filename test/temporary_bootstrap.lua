d6fe8162fe25f59de89707adc3613b5ef2b1e453
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-11-06 11:42:34 -0500

    Left comment in temporary bootstrap test. For now, configuration is not stubbed (needs to happen when loading the script the first time via do_file)

--- test/temporary_bootstrap.lua
+++ test/temporary_bootstrap.lua
@@ -3,8 +3,12 @@ TestTemporaryBootstrap = {}
 function TestTemporaryBootstrap:testRunVatsimbriefHelperAtOneFramePerSecond()
     flyWithLuaStub:reset()
     local vatsimbriefHelper = dofile("scripts/vatsimbrief-helper.lua")
+    -- For now, this won't trigger flight plan downloads because the add_macro call for initial control window visibility happened already.
+    -- Solution: Add a config file stub
+    vatsimbriefHelperPackageExport.test.Configuration.flightplan = {}
+    vatsimbriefHelperPackageExport.test.Configuration.flightplan.windowVisibility = "visible"
     vatsimbriefHelperPackageExport.test.Configuration.File.simbrief = {}
-    vatsimbriefHelperPackageExport.test.Configuration.File.simbrief.username = "<<<YOUR_SIMBRIEF_USERNAME_HERE>>>"
+    vatsimbriefHelperPackageExport.test.Configuration.File.simbrief.username = "<<<<USERNAME_HERE>>>>>"
     vatsimbriefHelperPackageExport.test.Configuration.File.flightplan = {}
     vatsimbriefHelperPackageExport.test.Configuration.File.flightplan.flightPlanTypesForDownload1TypeName = "vPilot"
     flyWithLuaStub:bootstrapAllMacros()
