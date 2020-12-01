86b897da463941298539ffafa148e22c1fb1520d
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-12-01 10:41:37 -0500

    Cleanup for release.

--- script_modules/vatsimbrief-helper/main_thread.lua
+++ script_modules/vatsimbrief-helper/main_thread.lua
@@ -3,6 +3,7 @@ local function emitVatsimDataUpdateEvent()
     emitVatsimDataUpdateEventInMainThreadSoon = true
 end
 
+TRACK_ISSUE("Tech Debt", "Replace specific vatsim update event method by generic MainThread.do(f) method.")
 local M = {}
 M.emitVatsimDataUpdateEvent = emitVatsimDataUpdateEvent
 M.loop = function()
